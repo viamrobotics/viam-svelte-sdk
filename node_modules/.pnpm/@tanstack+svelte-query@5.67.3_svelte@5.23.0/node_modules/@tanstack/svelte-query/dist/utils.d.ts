7a1d12d7fbfca8ebd0e4d1b0d9ccadb3f0be98a3
Micheal Parks <micheal.parks@outlook.com>
2025-03-13 16:15:22 -0400

    init

--- node_modules/.pnpm/@tanstack+svelte-query@5.67.3_svelte@5.23.0/node_modules/@tanstack/svelte-query/dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Readable } from 'svelte/store';
-import type { StoreOrVal } from './types.js';
-export declare function isSvelteStore<T extends object>(obj: StoreOrVal<T>): obj is Readable<T>;
-export declare function noop(): void;
-//# sourceMappingURL=utils.d.ts.map
--- node_modules/.pnpm/@tanstack+svelte-query@5.67.3_svelte@5.23.0/node_modules/@tanstack/svelte-query/dist/utils.js
+++
@@ -1,4 +0,0 @@
-export function isSvelteStore(obj) {
-    return 'subscribe' in obj && typeof obj.subscribe === 'function';
-}
-export function noop() { }
