1298c6ee9cea610160070542a0c6cc6dff31d982
Micheal Parks <micheal.parks@outlook.com>
2025-03-14 11:53:35 -0400

    cleanup

--- src/routes/configs.ts
+++ src/routes/configs.ts
@@ -1,11 +1,12 @@
 import type { DialConf } from '@viamrobotics/sdk';
 
 const parseConfigs = () => {
-  console.log(import.meta);
   const rawRobots = import.meta.env.VITE_CONFIGS;
+
   if (!rawRobots) {
     throw new Error('Cannot find configs. Please read the README.md for more info');
   }
+
   return JSON.parse(rawRobots);
 };
 
