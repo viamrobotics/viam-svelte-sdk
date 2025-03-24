4784d1d96d8d21e5a4b088760ed1150e8882e675
Micheal Parks <micheal.parks@outlook.com>
2025-03-24 17:06:22 -0400

    fix publish script

--- src/lib/__tests__/compare.spec.ts
+++ src/lib/__tests__/compare.spec.ts
@@ -1,4 +1,4 @@
-import { comparePartIds } from '$lib/compare';
+import { comparePartIds, isJsonEqual } from '$lib/compare';
 import { describe, it, expect } from 'vitest';
 
 describe('comparePartIds()', () => {
@@ -11,5 +11,57 @@ describe('comparePartIds()', () => {
     expect(added).toEqual(['id4']);
     expect(removed).toEqual(['id1']);
     expect(unchanged).toEqual(['id2', 'id3']);
+  });
+});
+
+describe('isJsonEqual()', () => {
+  it('returns true if two JSONs have equal values', () => {
+    const json1 = {
+      key1: true,
+      key2: 'value2',
+      key3: 3,
+      key4: { nested1: true },
+    };
+    const json2 = {
+      key1: true,
+      key2: 'value2',
+      key3: 3,
+      key4: { nested1: true },
+    };
+
+    expect(isJsonEqual(json1, json2)).toBe(true);
+  });
+
+  it('returns false if two JSONs do not have equal values', () => {
+    const json1 = {
+      key1: true,
+      key2: 'value2',
+      key3: 3,
+      key4: { nested1: true },
+    };
+    const json2 = {
+      key1: true,
+      key2: 'value2',
+      key3: 3,
+      key4: { nested1: false },
+    };
+
+    expect(isJsonEqual(json1, json2)).toBe(false);
+  });
+
+  it('returns false if two JSONs do not have the same keys', () => {
+    const json1 = {
+      key1: true,
+      key2: 'value2',
+      key3: 3,
+      key4: { nested1: true },
+    };
+    const json2 = {
+      key1: true,
+      key2: 'value2',
+      key3: 3,
+    };
+
+    expect(isJsonEqual(json1, json2)).toBe(false);
   });
 });
