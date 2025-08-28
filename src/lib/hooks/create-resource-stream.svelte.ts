fad0eb83a23f3c0afb23f0d97843b6fb85327229
Devin C <devin@viam.com>
2025-08-28 16:52:02 -0400

    some updates, still don't think types are right

--- src/lib/hooks/create-resource-stream.svelte.ts
+++ src/lib/hooks/create-resource-stream.svelte.ts
@@ -1,6 +1,8 @@
 import {
   experimental_streamedQuery as streamedQuery,
-  type QueryFunction,
+  createQuery,
+  type QueryObserverResult,
+  queryOptions as createQueryOptions,
 } from '@tanstack/svelte-query';
 import type { Resource } from '@viamrobotics/sdk';
 import { toStore, fromStore } from 'svelte/store';
@@ -9,19 +11,24 @@ import { useQueryLogger } from '../query-logger';
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type ArgumentsType<T> = T extends (...args: infer U) => any ? U : never;
 
-export type ResolvedReturnType<T> = T extends (
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ...args: any[]
-) => Promise<infer R>
-  ? R
-  : never;
-
 interface QueryOptions {
   // enabled defaults to true if unspecified
   enabled?: boolean;
   refetchMode?: 'append' | 'reset' | 'replace';
   maxChunks?: number;
 }
+
+export type ResolvedReturnType<T> = T extends (
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ...args: any[]
+) => infer R
+  ? R
+  : never;
+
+type QueryResult<T extends Resource, K extends keyof T> = QueryObserverResult<
+  ResolvedReturnType<T[K]>[],
+  Error
+>;
 
 export const createResourceStream = <T extends Resource, K extends keyof T>(
   client: { current: T | undefined },
@@ -32,7 +39,7 @@ export const createResourceStream = <T extends Resource, K extends keyof T>(
         options?: (() => QueryOptions) | QueryOptions,
       ]
     | [options?: (() => QueryOptions) | QueryOptions]
-): QueryFunction<ResolvedReturnType<T[K]>[], typeof queryKey> => {
+): { current: QueryResult<T, K> } => {
   const debug = useQueryLogger();
 
   let [args, options] = additional;
@@ -45,10 +52,10 @@ export const createResourceStream = <T extends Resource, K extends keyof T>(
   const _options = $derived(
     typeof options === 'function' ? options() : options
   );
+
   const _args = $derived(typeof args === 'function' ? args() : args);
   const name = $derived(client.current?.name);
   const methodName = $derived(String(method));
-
   const queryKey = $derived([
     'viam-svelte-sdk',
     'partID',
@@ -59,40 +66,48 @@ export const createResourceStream = <T extends Resource, K extends keyof T>(
     ...(_args ? [_args] : []),
   ]);
 
-  const queryOptions = $derived({
-    queryKey,
-    queryFn: async () => {
-      const clientFunc = client.current?.[method];
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type, @typescript-eslint/no-explicit-any
+  function processStream() {
+    const clientFunc = client.current?.[method];
 
-      if (typeof clientFunc !== 'function') {
-        throw new TypeError(
-          `${String(method)} is not a method on the resource client.`
-        );
-      }
+    if (typeof clientFunc !== 'function') {
+      throw new TypeError(
+        `${String(method)} is not a method on the resource client.`
+      );
+    }
 
-      const logger = debug.createLogger();
-      logger('REQ', name, methodName, _args);
+    const logger = debug.createLogger();
+    logger('REQ', name, methodName, _args);
 
-      try {
-        const response = (await clientFunc?.apply(
-          client.current,
-          _args
-        )) as AsyncIterable<ResolvedReturnType<T[K]>>;
+    try {
+      const response = clientFunc?.apply(
+        client.current,
+        _args
+      ) as AsyncGenerator<ResolvedReturnType<T[K]>>;
+      console.log('response', typeof response);
 
-        logger('RES', name, methodName, response);
-        return response;
-      } catch (error) {
-        logger('ERR', name, methodName, error);
-        throw error;
-      }
-    },
-    ..._options,
-  });
+      logger('RES', name, methodName, response);
+      return {
+        async *[Symbol.asyncIterator]() {
+          yield* response;
+        },
+      };
+    } catch (error) {
+      logger('ERR', name, methodName, error);
+      throw error;
+    }
+  }
 
-  const queryOptionsStore = fromStore(toStore(() => queryOptions));
-  const query = streamedQuery<ResolvedReturnType<T[K]>, typeof queryKey>(
-    queryOptionsStore.current
+  const queryOptions = $derived(
+    createQueryOptions({
+      queryKey,
+      enabled: client.current !== undefined && _options?.enabled !== false,
+      queryFn: streamedQuery<ResolvedReturnType<T[K]>>({
+        queryFn: processStream,
+        ..._options,
+      }),
+    })
   );
 
-  return query;
+  return fromStore(createQuery(toStore(() => queryOptions)));
 };
