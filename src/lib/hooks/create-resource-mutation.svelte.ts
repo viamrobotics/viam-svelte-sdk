cffb129d96bfd4f5f1c9d0ff9080895c94cd166b
Matthew MacFarquhar <matthew.macfarquhar@gmail.com>
2026-03-10 13:55:18 -0400

    feat: take in optional queryKey to do optimistic updates on resource mutation

--- src/lib/hooks/create-resource-mutation.svelte.ts
+++ src/lib/hooks/create-resource-mutation.svelte.ts
@@ -1,4 +1,4 @@
-import { createMutation, type MutationOptions } from '@tanstack/svelte-query';
+import { createMutation, type MutationOptions, type QueryOptions, useQueryClient } from '@tanstack/svelte-query';
 import type { Resource } from '@viamrobotics/sdk';
 
 import type {
@@ -9,13 +9,14 @@ import { useQueryLogger } from '$lib/query-logger';
 
 export const createResourceMutation = <T extends Resource, K extends keyof T>(
   client: { current: T | undefined },
-  method: K
+  method: K,
+  queryKey?: (string | QueryOptions | ArgumentsType<T[K]> | undefined)[]
 ) => {
   type MutArgs = ArgumentsType<T[K]>;
   type MutReturn = ResolvedReturnType<T[K]>;
 
   const debug = useQueryLogger();
-
+  const queryClient = useQueryClient();
   const name = $derived(client.current?.name);
   const methodName = $derived(String(method));
 
@@ -53,6 +54,32 @@ export const createResourceMutation = <T extends Resource, K extends keyof T>(
         throw error;
       }
     },
+    onMutate: async (request) => {
+      if (!queryKey) {
+        return;
+      }
+
+      
+      await queryClient.cancelQueries({ queryKey });
+      const previousData = await queryClient.getQueryData(queryKey);
+
+      queryClient.setQueryData(queryKey, request);
+
+      return { previousData };
+    },
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    onError: (_error: unknown, _request: unknown, context: any) => {
+      if (context?.previousData && queryKey) {
+        queryClient.setQueryData(queryKey, context.previousData);
+      }
+    },
+    onSettled: () => {
+      if (!queryKey) {
+        return;
+      }
+
+      queryClient.invalidateQueries({ queryKey });
+    }
   } satisfies MutationOptions<MutReturn, Error, MutArgs>);
 
   return createMutation(() => mutationOptions);
