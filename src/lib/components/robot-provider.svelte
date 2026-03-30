ede6b40ee88bcb2a5196279165a317477d39c8a4
Devin C <devin@viam.com>
2026-03-30 10:12:01 -0400

    use derived

--- src/lib/components/robot-provider.svelte
+++ src/lib/components/robot-provider.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { untrack, type Snippet } from 'svelte';
+import { type Snippet } from 'svelte';
 import {
   QueryClientProvider,
   QueryClient,
@@ -28,7 +28,7 @@ let {
   children,
 }: Props = $props();
 
-export const client = new QueryClient(untrack(() => config));
+const client = $derived(new QueryClient(config));
 
 $effect(() => {
   if (logLevel !== undefined) {
